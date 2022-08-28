2dc33c5c1cf30174cb3a95d83481f4f0f499d44d
blackpandan <faithgstc54@gmail.com>
2022-08-28 22:23:08 +0100

    NEW: added install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+ #! /bin/bash
+
+sudo apt install zsh
+
+chsh -s $(which zsh)
+
+sudo apt install tmux
+
+sudo apt install powerline
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
