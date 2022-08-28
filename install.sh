37022393d1fb1b3e12fa22f1189c5da637210a89
blackpandan <faithgstc54@gmail.com>
2022-08-28 22:27:30 +0100

    added stow to install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sudo apt install stow
